0791d81b71b3ba6fa67d2baba2fe72fa6554995d
TogariKatuya <k.togari.cingroup2012@gmail.com>
2025-02-12 17:50:38 +0900

    Vue属性について

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,11 @@
-<sucript setup>
+<script setup>
+import { ref } from 'vue'
 
-  console titleref('Vue.js Course')
-  let price =ref(9,.99)
-  function increment(){
-    price.value += 1
-    increment.age += 1
-    increment.bio = 'h1'
-    increment.sns.twitter = 'hello'
-    increment.email = ref('info@example.com')
-  }
-  const info = ref({
-    Students: 1000,
-    rating: 4
-  })
-  const increment = reactive({
-    name: 'Yoshipi'
-    age:25,
-    sns:{
-      twitter:'@__yoshipi__'
-      youtube:'`yoshipi`'
-    }
-  })
-  instructor.bio = 'hello'
-  const items = reactive([ref(1),ref(2),ref(3)])
-  console.log(info.value.Students)
-
-</sucript>
+const count = ref(2)
+const count2 = ref(4)
+</script>
 <template>
-  <h1>Title:{{ title }}</h1>
-  <h2>Price:${{ price - 1 }}</h2>
-  <button @clck="increment">button</button>
-  <h2>Students:{{ info.Students }}</h2>
-  <h2>Instructor age: {{ instructor.age }}</h2>
-  <h2>Instructor bio: {{ instructor.bio }}</h2>
-  <h2>Instructor SNS Twitter:{{ instructor.sns.twitter }}</h2>
-  <h2>Instructor email: {{ instructor.email }}</h2>
-  <h2>Coures Info Section: {{ coureInfo.sections + 1 }}</h2>
+  <div>{{ count + count2 }}</div>
+  <div>{{ count > 3 ? 'Yes' : 'No' }}</div>
+  <div>{{ count }}</div>
 </template>
