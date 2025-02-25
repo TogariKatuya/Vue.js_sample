778c7b3b5dc9737aa4d974a87fcc0e3295ccb3e7
TogariKatuya <k.togari.cingroup2012@gmail.com>
2025-02-25 15:47:22 +0900

    Todo機能実装

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
-<script setup>
-import { ref } from 'vue'
-
-const count = ref(0)
-</script>
 <template>
-  <p>{{ count }}</p>
-  <div @click="count++">
-    <button @click="$event.stopPropagation()">button</button>
+  <div>
+    <h3>Todo</h3>
+    <input v-model="newTodo" placeholder="Input here..." />
+    <button v-on:click="addTodo()">ADD</button>
+    <h5>Todo list</h5>
+    <ul>
+      <li v-for="(todo, i) in todos" v-bind:key="i">
+        {{ todo }}
+        <button v-on:click="deleteTodo(i)">DEL</button>
+      </li>
+    </ul>
   </div>
-  <a href="https://vuejs.org" @click.prevent="$event.preventDefault()">Vue.js</a>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: '',
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo === '') return
+      this.todos.push(this.newTodo)
+      this.newTodo = ''
+    },
+    deleteTodo(i) {
+      this.todos.splice(i, 1)
+    },
+  },
+}
+</script>
